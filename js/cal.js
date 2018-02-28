1480391acbc54094fa25116cf932ca5a056b2156
Yimu Gao <delphox0121@gmail.com>
2018-02-28 03:38:16 -0800

    add google signin/out, get freebusy time from other users and create event while invites other users currently on the calendar

--- js/cal.js
+++ js/cal.js
@@ -1,19 +1,56 @@
 var API_KEY = 'AIzaSyDvCRXC4jHo6OR0Gz2rc3rseF9ZdncCElA';
-var users = ['gardevoir0121@gmail.com', 'yimugao@udel.edu', 'dkaw'];
+var CLIENT_ID =
+  '1053922014560-ieg507hrun9brjopinvau268bfpj63oc.apps.googleusercontent.com';
+var users = [];
 var MASTER_COLOR = 'bg-danger';
-//var COLORS = ['bg-danger', 'bg-success', 'bg-primary', 'bg-warning'];
-var COLORS = ['red', 'green', 'indigo', 'orange'];
+var COLORS = ['danger', 'success', 'info', 'warning'];
+var DISCOVERY_DOCS = [
+  'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
+];
+var SCOPES = 'https://www.googleapis.com/auth/calendar';
+//var COLORS = ['red', 'green', 'indigo', 'orange'];
 
-function showUsers() {
-  appendPre('user-list', 'User list:');
-  for (var i in users) {
-    appendPre('user-list', users[i]);
-  }
+var authorizeButton = $('#authorize-button');
+var signoutButton = $('#signout-button');
+
+let PRIMARY = 'primary';
+let OTHERS = 'others';
+
+let TIMEMIN_FOR_SELF = '2018-02-01T00:00:00-07:00';
+let TIMEMIN_FOR_OTHERS = '2018-02-01T00:00:00-07:00';
+let TIMEMAX_FOR_OTHERS = '2018-04-01T00:00:00-07:00';
+
+function addUsers(user) {
+  $('#user-list')
+    .find('ul')
+    .append(
+      '<li class="list-group-item list-group-item-action list-group-item-warning">' +
+        user +
+        '</li>'
+    );
 }
 
-$(document).ready(function() {
-  //showUsers();
-  //gapi.load('client', start);
+$(function() {
+  gapi.load('client:auth2', initClient);
+  $('#add-user').on('submit', function(event) {
+    event.preventDefault();
+    //console.log(event);
+    console.log(event.target['0'].value);
+    testUser(event.target['0'].value);
+    $('#add-user').trigger('reset');
+  });
+  //console.log('ready to load calendar');
+});
+
+function createAttendeesFromUsersList() {
+  return users.map(function(user) {
+    return {
+      email: user
+    };
+  });
+}
+
+function initializeCalendar() {
   $('#calendar').fullCalendar({
     header: {
       left: 'prev,next today',
@@ -33,7 +70,7 @@ $(document).ready(function() {
           start: start,
           end: end,
           editable: true,
-          className: 'text-white'
+          className: 'text-white bg-success'
         };
         $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
       }
@@ -41,23 +78,6 @@ $(document).ready(function() {
     },
     displayEventTime: true,
     timeFormat: 'h:mmt',
-    googleCalendarApiKey: API_KEY,
-    events: [
-      {
-        title: 'seewhathappens',
-        start: '2018-02-17T17:30:00',
-        end: '2018-02-17T18:30:00',
-        editable: true,
-        color: 'default',
-        className: 'text-white'
-      }
-    ],
-    eventSources: users.map(function(user, i) {
-      return {
-        googleCalendarId: user,
-        color: COLORS[i % COLORS.length]
-      };
-    }),
     eventClick: function(event, jsEvent, view) {
       jsEvent.preventDefault();
       // opens events in a popup window
@@ -73,9 +93,192 @@ $(document).ready(function() {
         event.title;
       console.log('event: ', event);
       $('.modal-body').html(eventDetails);
+      $('#create-event').on(
+        'click',
+        createEvent({
+          sendNotifications: true,
+          calendarId: 'primary',
+          summary: event.title,
+          start: {
+            dateTime: event.start._d
+          },
+          end: {
+            dateTime: event.end === null ? '' : event.end._d
+          },
+          attendees: createAttendeesFromUsersList()
+        })
+      );
       $('#bookEventModal').modal();
       //console.log('jsEvent: ', jsEvent);
       //console.log('view: ', view);
     }
   });
-});
+  console.log('calendar initialized.');
+}
+
+function initClient() {
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    })
+    .then(function() {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      //console.log(authorizeButton);
+      //console.log(document.getElementById('authorize-button'));
+      authorizeButton.click(handleAuthClick);
+      signoutButton.click(handleSignoutClick);
+    });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    // log in, initialize calendar, get events from primary calendars first
+    // and enable submit email address function
+    console.log('signed in.');
+    console.log(
+      'current user: ',
+      gapi.auth2
+        .getAuthInstance()
+        .currentUser.get()
+        .getBasicProfile()
+        .getEmail()
+    );
+    authorizeButton.addClass('d-none');
+    signoutButton.removeClass('d-none');
+    $('#submit-email').prop('disabled', false);
+    $('#input-email').prop('disabled', false);
+    initializeCalendar();
+    $('#calendar').fullCalendar('render');
+    console.log('try to get events from primary calendar');
+    gapi.client.calendar.events
+      .list({
+        calendarId: 'primary',
+        timeMin: TIMEMIN_FOR_SELF,
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 30,
+        orderBy: 'startTime'
+      })
+      .then(function(response) {
+        console.log('got events.');
+        $('#calendar').fullCalendar(
+          'addEventSource',
+          createEventObjects(response.result.items, PRIMARY)
+        );
+        $('#calendar').removeClass('d-none');
+      });
+  } else {
+    console.log('signed out.');
+    // when log out destory calendar
+    // disable form
+    $('#submit-email').prop('disabled', true);
+    $('#input-email').prop('disabled', true);
+    authorizeButton.removeClass('d-none');
+    signoutButton.addClass('d-none');
+    $('#calendar').addClass('d-none');
+    $('#calendar').fullCalendar('destroy');
+  }
+}
+
+function createEventObjects(events, type) {
+  return events.map(function(event) {
+    if (type == PRIMARY) {
+      return {
+        id: event.id,
+        title: event.summary,
+        start: event.start.dateTime,
+        end: event.end.dateTime
+      };
+    } else if (type == OTHERS) {
+      return {
+        title: 'busy',
+        start: event.start,
+        end: event.end,
+        className: 'bg-warning'
+      };
+    } else return {};
+  });
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  event.preventDefault();
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  event.preventDefault();
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+function createEvent(newEvent) {
+  return function() {
+    console.log('create event clicked.');
+    console.log('new event:', newEvent);
+    gapi.client.calendar.events.insert(newEvent).then(function(response) {
+      console.log(response);
+      if (response.status == 200) {
+        console.log('created!');
+        $('#bookEventModal').modal('hide');
+      } else {
+        console.log('something went wrong!');
+      }
+    });
+  };
+}
+
+function testUser(user) {
+  if (
+    user ==
+    gapi.auth2
+      .getAuthInstance()
+      .currentUser.get()
+      .getBasicProfile()
+      .getEmail()
+  )
+    return;
+  console.log('trying to get query of busy time.');
+  gapi.client.calendar.freebusy
+    .query({
+      timeMin: TIMEMIN_FOR_OTHERS,
+      timeMax: TIMEMAX_FOR_OTHERS,
+      items: [
+        {
+          id: user
+        }
+      ]
+    })
+    .then(function(response) {
+      console.log(response);
+      if (response.result.calendars[user].errors) {
+        console.log(
+          'not valid user or no access to his/her freebusy time: ',
+          user
+        );
+        return;
+      }
+      // we can access this user's freebusy times
+      addUsers(user);
+      users.push(user);
+      $('#calendar').fullCalendar(
+        'addEventSource',
+        createEventObjects(response.result.calendars[user].busy, OTHERS)
+      );
+      console.log('events added');
+    });
+}
